0b773181c5181799b29a8e35ac58705ca8f5ea68
Shaelyn26 <shaelynb0405@gmail.com>
2025-03-15 15:49:53 +0000

    aligned list to center added task

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [choresArray, setChoresArray] = useState([
+    "Walk my dogs",
+    "Do my homework",
+    "Wash the dishes",
+    "Clean the house",
+    "Wash the car",
+  ]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const deleteToDo = (itemToDelete) => {
+    const result = choresArray.filter((item) => item != itemToDelete);
+    setChoresArray(result);
+  };
+
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>To-Do List</h1>
+      </div>
+      <ul>
+        {choresArray.map((item) => {
+          return (
+            <div className="list d-flex mx-auto">
+              {item}
+              <span className="delete-btn mx-2" onClick={() => deleteToDo(item)} onMouseOver={(e) => e.target.choresArray = "hover"}>
+                x
+              </span>
+            </div>
+          );
+        })}
+      </ul>
+      <input className="text d-flex mx-auto"
+      type="text"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+          const newChoreArray = [...choresArray,e.target.value]
+            setChoresArray(newChoreArray)
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default Home;
